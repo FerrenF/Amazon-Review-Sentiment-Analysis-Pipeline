5a4d174603d8382e26b79cb94cff3cbb31f7b760
Hunter W. <kfateweaver@gmail.com>
2025-04-09 18:21:39 -0400

    oops

--- src/main.py
+++ src/main.py
@@ -5,14 +5,14 @@ from src.steps.load_dataset import LoadDatasetStep
 from src.utils.write_dataset import write_dataset
 
 
-def loading_stage_finished_callback(stage: Stage, data: dict):
-    write_dataset(stage, "data", data)
+def loading_stage_finished_callback(stage: Stage, _data: dict):
+    write_dataset(stage, "data", _data)
 
 
 project_stages = [
     Stage("loading", [
         LoadDatasetStep()
-    ],  on_complete=loading_stage_finished_callback)
+    ],  on_complete=loading_stage_finished_callback),
     Stage("cleaning", [
 
     ]),
