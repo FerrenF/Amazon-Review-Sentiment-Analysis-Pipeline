2587d65b207cb02a3402601c16de9ef972ed7807
km34622 <139386338+khalilxfl@users.noreply.github.com>
2025-04-10 18:33:24 -0400

    Implemented cata cleaning based on text length

--- src/main.py
+++ src/main.py
@@ -1,12 +1,16 @@
 
-from core.pipeline import Pipeline
+from src.core.pipeline import Pipeline
 from src.core.stage import Stage
 from src.steps.load_dataset import LoadDatasetStep
+from src.steps.clean_dataset import CleanDatasetStep
 from src.utils.write_dataset import write_dataset
 
 
 def loading_stage_finished_callback(stage: Stage, _data: dict):
     write_dataset(stage, "data", _data)
+def cleaning_stage_finished_callback(stage: Stage, _data: dict):
+    write_dataset(stage, "data", _data)
+
 
 
 project_stages = [
@@ -14,8 +18,9 @@ project_stages = [
         LoadDatasetStep()
     ],  on_complete=loading_stage_finished_callback),
     Stage("cleaning", [
+        CleanDatasetStep(min_length = 10, max_length = 300)
 
-    ]),
+    ], on_complete=cleaning_stage_finished_callback),
     Stage("processing", [
 
     ]),
---
+++ src/steps/clean_dataset.py
@@ -0,0 +1,63 @@
+import json
+import os
+import pathlib
+
+from src.core.step import Step
+from src.project_common import *
+
+import pandas as pd
+# We want to extend our Step class, and ensure that we have defined all the methods within Step so that our base
+# pipeline can treat all of these steps the same way.
+
+
+class CleanDatasetStep(Step):
+    name = "clean_dataset"
+    
+    def __init__(self, min_length: int, max_length: int):
+        #Initializing the min and max attributes
+        self.min_length = min_length
+        self.max_length = max_length
+
+    
+    def filter_length(self, text: str) -> bool:
+        length = len(text)
+        
+        if length >= self.min_length and length <= self.max_length:
+            return True
+        else:
+            return False
+
+
+    def run(self, data: dict) -> dict:
+        """
+        This step performs cleaning on data by removing punctuation and filtering short/long text
+
+        :param data: A dict, whose 'dataset' contains the data to be cleaned
+        :return: the modified 'data' dict
+        """
+        
+        df = data.get("dataset")
+        if df is None:
+            raise ValueError("No dataset found in 'data' parameter")
+        
+        logging.info(f"Total rows at the beginning of cleaning: {len(df)}")
+        
+        #Stores the datapoints that are suitable length
+        filtered_datapoints = []
+        
+        #Loop through the dataset to filter out text that are too long or too short
+        for text in df['text']:
+            if self.filter_length(text):
+                filtered_datapoints.append(text)
+        
+        #Iterate through dataframe and remove data points that dont meet the length filter
+        filtered_df = df[df['text'].apply(self.filter_length)]
+        
+        rows_removed = len(df) - len(filtered_df)
+        logging.info(f"Cleaning complete, {rows_removed} rows removed and {len(filtered_df)} rows remaining.")
+        
+        data["dataset"] = filtered_df
+        
+        return data
+        
+    
